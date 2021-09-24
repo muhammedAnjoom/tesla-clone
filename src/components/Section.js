0c7b39abf768c1aebdf509258ac42c616c005c2d
muhammedAnjoom <anjoompa42003@gmail.com>
2021-09-24 20:40:04 +0530

    ADD:redux for cars

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,26 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade';
 
 function Section({title, description, leftbtnText, rightbtnText, backgroundImg}) {
     
     return (
         <div>
             <Wrap bgIMage={backgroundImg}>
-              <ItemText>
-                  <h1>{title}</h1>
-                  <p>{description}</p>
-              </ItemText>
+                <Fade bottom>
+                     <ItemText>
+                       <h1>{title}</h1>
+                       <p>{description}</p>
+                      </ItemText>
+               </Fade>
               <Button>
-                   <ButtonGroup>
-                        <LeftButton>
-                           {leftbtnText}
-                        </LeftButton>
-                        { rightbtnText &&
-                            <RightButton>
-                           {rightbtnText}
-                        </RightButton>
-                        }
-                   </ButtonGroup>
+                   <Fade bottom>
+                      <ButtonGroup>
+                            <LeftButton>
+                              {leftbtnText}
+                            </LeftButton>
+                           { rightbtnText &&
+                              <RightButton>
+                             {  rightbtnText}
+                              </RightButton>
+                           }
+                      </ButtonGroup>
+                   </Fade>
                   <DownArrow src="images/down-arrow.svg"/>
               </Button>
             </Wrap>
@@ -30,6 +35,7 @@ function Section({title, description, leftbtnText, rightbtnText, backgroundImg})
 
 export default Section
  const Wrap = styled.div`
+      z-index: 10;
       width; 100vw;
       height: 100vh;
       background-size: cover;
@@ -45,7 +51,7 @@ export default Section
  const ItemText = styled.div`
       padding-top: 15vh;
       text-align: center;
-
+      z-index: -1;
       
  `
  const ButtonGroup = styled.div`
